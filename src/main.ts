657c5a5ce404c8765c84f86bded575660417c9b1
Jay Sherman <jay@jaysherman.com>
2025-11-18 21:51:58 -0800

    feat: enable CORS for specific frontend origins and log application URL on startup

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for the frontend
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://medley-frontend-poc.s3-website-us-west-2.amazonaws.com',
+    ],
+    credentials: true,
+  });
+
   const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');  // '0.0.0.0' is important in containers
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
